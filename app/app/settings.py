883a8a7bafbdf81538aab5bf7ba3d6befda2eb0b
Biju Tholath <biju.tholath@gmail.com>
2025-02-12 15:39:56 -0800

    Configured wait_for_db

--- app/app/settings.py
+++ app/app/settings.py
@@ -128,3 +128,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Settings Auth_User_Modal configuration
+AUTH_USER_MODEL = 'core.User'
